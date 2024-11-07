7c36e5d0b1068b79e50d34b37dfe806aa41259c4
Maks <maks.kvitko.333@gmail.com>
2024-11-07 11:57:09 +0000

    remove duplicate dir

--- lab2/node-app/src/products/products.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Category } from '../categories/category.entity';
-
-export interface Product {
-    id?: number;
-    name: string;
-    description: string;
-    price: number;
-    category?: Category;
-}
